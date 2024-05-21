7eb6422dc43a76e8af3ee81d9dfd29fe2d1ea664
Al Francis B. Paz <afpaz@gbox.adnu.edu.ph>
2024-05-21 15:04:00 +0800

    dropdown 1

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,2 +1,4 @@
 class Bus < ApplicationRecord
+    has_many: drivers
+    has_many: trips
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,5 @@
 class Trip < ApplicationRecord
   belongs_to :bus
+  has_many :reservations, dependent: :delete_all
+  has_many :passengers, through: :reservations, dependent: :delete_all
 end
